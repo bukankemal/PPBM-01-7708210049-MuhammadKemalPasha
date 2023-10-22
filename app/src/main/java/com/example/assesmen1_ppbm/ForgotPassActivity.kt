d65ea21a077dd95e7e2e6b5433fab179e43bf635
BuildTools <unconfigured@null.spigotmc.org>
2023-10-23 06:34:09 +0700

    added function to send code button

--- app/src/main/java/com/example/assesmen1_ppbm/ForgotPassActivity.kt
+++ app/src/main/java/com/example/assesmen1_ppbm/ForgotPassActivity.kt
@@ -1,11 +1,19 @@
 package com.example.assesmen1_ppbm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ForgotPassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_pass)
+
+        val btnNextPage = findViewById<Button>(R.id.send_code)
+        btnNextPage.setOnClickListener{
+            val _intent = Intent(this, MainActivity::class.java)
+            startActivity(_intent)
+        }
     }
 }
